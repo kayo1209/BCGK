f6d4c97aac3c3f64c17c289131c703b08dba761a
Đăng Khôi <126789364+kayo1209@users.noreply.github.com>
2023-04-12 14:31:47 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,77 @@
+// let types = new Map([
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+//   [],
+// ]);
+
+switch (type) {
+  case "normal":
+    style.backgroundColor = "#FFFFFF";
+    break;
+  case "fire":
+    style.backgroundColor = "#A52A2A";
+    break;
+  case "water":
+    style.backgroundColor = "#00008B    ";
+    break;
+  case "grass":
+    style.backgroundColor = "#006400";
+    break;
+  case "flying":
+    style.backgroundColor = "#F0F8FF";
+    break;
+  case "fighting":
+    style.backgroundColor = "#DC143C";
+    break;
+  case "poison":
+    style.backgroundColor = "DarkMagenta";
+    break;
+  case "electric":
+    style.backgroundColor = "#FFD700";
+    break;
+  case "ground":
+    style.backgroundColor = "#8B4513";
+    break;
+  case "rock":
+    style.backgroundColor = "#708090";
+    break;
+  case "Psychic":
+    style.backgroundColor = "#800080";
+    break;
+  case "ice":
+    style.backgroundColor = "#FFFAFA";
+    break;
+  case "bug":
+    style.backgroundColor = "#3CB371";
+    break;
+  case "ghost":
+    style.backgroundColor = "#F8F8FF";
+    break;
+  case "Steel":
+    style.backgroundColor = "#A9A9A9";
+    break;
+  case "dragon":
+    style.backgroundColor = "#B22222";
+    break;
+  case "dark":
+    style.backgroundColor = "#778899";
+    break;
+  case "fairy":
+    style.backgroundColor = "#5F9EA0";
+    break;
+}
